7cadc0beaddfcaa0b5a4492dfbf864828833ebea
AnastasiaProkofieva <anastasia.beetroot@gmail.com>
2025-05-20 02:39:42 +0300

    added html and css files to project

--- notes/urls.py
+++ notes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import hello_world
+from .views import hello_world, test_data
 
 urlpatterns = [
     path('hello/', hello_world),
+    path('notes/', test_data),
+
 ]
--- notes/views.py
+++ notes/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 def hello_world(request):
      return HttpResponse('Hello from Notes app.')
+
+def test_data(request):
+    context = {
+        'title': 'Notes',
+        'items': ['Make python hometasks', 'Cook dinner', 'Feed stray cats']
+    }
+    return render(request, 'my_notes.html', context)
